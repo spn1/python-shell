591583d8bf1b8a4702437ae13f50ebdb163e48b7
snewton27 <spencer.newton@sky.uk>
2025-01-06 11:40:55 +0000

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 import sys
 
+def echo(args):
+    print(' '.join(args))
+
+commands = {
+   'echo': echo,
+}
+
 
 def main():
     while True:
         sys.stdout.write("$ ")
-        command = input().split()
+        args = input().split(' ')
+        # print(args)
+        command = args[0]
 
-        if command[0] == 'exit':
-            return command[1]
+        if command in commands:
+            commands[command](args[1:])
+        elif command == 'exit':
+            return args[1]
+        else:
+            print('{command}: command not found'.format(command=command))
 
-        print('{command}: command not found'.format(command=command[0]))
 
 
 if __name__ == "__main__":
